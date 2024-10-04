ba398e4cf0bcf49abf62440d77af0e63b8a18583
Jmerc89 <JdMercer1@student.fullsail.edu>
2024-10-04 16:28:47 -0500

    Added Chaser to Main

--- UnityProject/My project/Assets/Scripts/damage.cs
+++ UnityProject/My project/Assets/Scripts/damage.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class damage : MonoBehaviour
 {
-    [SerializeField] enum damageType { bullet, stationary};
+    [SerializeField] enum damageType { bullet, stationary, chaser};
     [SerializeField] damageType type;
     [SerializeField] Rigidbody rb;
 
@@ -14,10 +14,14 @@ public class damage : MonoBehaviour
     [SerializeField] int destroyTime;
     // Start is called before the first frame update
     void Start()
-    {
+    {   // standard bullets just travel in a straight line 
         if(type == damageType.bullet)
         {
             rb.velocity = transform.forward * speed;
+            Destroy(gameObject, destroyTime);
+        }
+        if(type==damageType.chaser)
+        {
             Destroy(gameObject, destroyTime);
         }
     }
@@ -35,6 +39,16 @@ public class damage : MonoBehaviour
         {
             dmg.takeDamage(damageAmount);
         }
+        else if(type==damageType.bullet||type==damageType.chaser)
+        { 
         Destroy(gameObject);
+        }
+    }
+    private void Update()
+    {
+        if(type==damageType.chaser)
+        {                                // gets player postions           //bullet position   //always normalize to keep scales reliable
+            rb.velocity = (GameManager.instance.player.transform.position - transform.position).normalized*speed*Time.deltaTime;
+        }
     }
 }
