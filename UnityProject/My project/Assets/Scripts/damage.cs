0ed6fb03967382fa34f9fcb533080dc78e435394
Grey <144875524+Greyscarr@users.noreply.github.com>
2024-10-07 13:15:52 -0500

    Fixed Errors w/Orion's Build and Mine merged as currently is.

--- UnityProject/My project/Assets/Scripts/damage.cs
+++ UnityProject/My project/Assets/Scripts/damage.cs
@@ -1,11 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
 using UnityEngine;
 
 public class damage : MonoBehaviour
 {
-    [SerializeField] enum damageType { bullet, stationary, chaser, arrow};
+    [SerializeField] enum damageType { bullet, stationary, chaser, arrow, ghostOrb};
+
     [SerializeField] damageType type;
     [SerializeField] Rigidbody rb;
 
@@ -15,29 +14,36 @@ public class damage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   // standard bullets just travel in a straight line 
-        if(type == damageType.bullet)
+        if (type == damageType.bullet)
         {
             rb.velocity = transform.forward * speed;
             Destroy(gameObject, destroyTime);
         }
-        if(type==damageType.chaser)
+        if (type == damageType.chaser)
         {
             Destroy(gameObject, destroyTime);
         }
-        if(type==damageType.arrow)//Should just travel in a straight line.
+
+        if(type==damageType.arrow)
+        {
+            rb.velocity = transform.forward * speed;
+            Destroy(gameObject, destroyTime);
+        }
+
+        if (type == damageType.ghostOrb)
         {
             rb.velocity = transform.forward * speed;
             Destroy(gameObject, destroyTime);
         }
     }
 
-  private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.isTrigger) return;
 
         IDamage dmg = other.GetComponent<IDamage>();
 
-        if(dmg != null )
+        if (dmg != null)
         {
             dmg.takeDamage(damageAmount);
         }
@@ -54,9 +60,9 @@ public class damage : MonoBehaviour
     }
     private void Update()
     {
-        if(type==damageType.chaser)
+        if (type == damageType.chaser)
         {                                // gets player postions           //bullet position   //always normalize to keep scales reliable
-            rb.velocity = (GameManager.instance.player.transform.position - transform.position).normalized*speed*Time.deltaTime;
+            rb.velocity = (GameManager.instance.player.transform.position - transform.position).normalized * speed * Time.deltaTime;
         }
     }
 }
