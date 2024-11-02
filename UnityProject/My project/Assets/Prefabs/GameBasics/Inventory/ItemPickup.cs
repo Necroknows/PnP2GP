10773cfe6e1b85c0b664bff37013912d042c8362
Aaron Goodwin <105810643+aggoodwin1988@users.noreply.github.com>
2024-11-02 13:56:10 -0400

    Inventory Build
    Open/Close w/ I key
    Aim + LMB adds item to item list on inventoryManager

---
+++ UnityProject/My project/Assets/Prefabs/GameBasics/Inventory/ItemPickup.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public Item item;
+
+    void Pickup()
+    {
+        InventoryManager.instance.AddItem(item);
+        Destroy(gameObject);
+    }
+
+    private void OnMouseDown()
+    {
+        Pickup();
+    }
+}
+//END
